7e3a41fcca744f59b8215ad5656e70a8b0a4627c
mattbalmer <signup@mattbalmer.com>
2019-01-19 20:48:08 +0000

    Europa

--- docs/bundle.js
+++ docs/bundle.js
@@ -91,33 +91,16 @@
   !*** ./source/challenges.ts ***!
   \******************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = __webpack_require__(/*! utils */ "./source/utils.ts");
-const data_1 = __webpack_require__(/*! data */ "./source/data.ts");
-let id = 0;
-exports.newID = () => {
-    return ++id;
-};
-exports.generate = () => {
-    const pack = utils_1.randValue(data_1.packs);
-    const randWeapons = utils_1.randSubmap(Object.assign({}, utils_1.randSubmap(data_1.pistols, 0, 1), utils_1.randSubmap(data_1.shotguns, 1, 2), utils_1.randSubmap(data_1.smgs, 1, 2), utils_1.randSubmap(data_1.ars, 1, 2), utils_1.randSubmap(data_1.snipers, 1, 2)), 2, 4);
-    return {
-        pack: pack,
-        weapons: [pack.weapon, ...Object.values(randWeapons)],
-    };
-};
 
 
 /***/ }),
 
-/***/ "./source/data.ts":
-/*!************************!*\
-  !*** ./source/data.ts ***!
-  \************************/
+/***/ "./source/data/dione.ts":
+/*!******************************!*\
+  !*** ./source/data/dione.ts ***!
+  \******************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -166,6 +149,92 @@ exports.packs = {
 
 /***/ }),
 
+/***/ "./source/data/europa.ts":
+/*!*******************************!*\
+  !*** ./source/data/europa.ts ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const types_1 = __webpack_require__(/*! types */ "./source/types.ts");
+const _ = {
+    pack: (name, weapon) => ({ name, weapon }),
+    weapon: (name, type, ammo, crate = false) => ({ name, type, ammo, crate }),
+};
+exports.smgs = {
+    'MPX': _.weapon('MPX', types_1.WeaponType.SMG, types_1.Ammo._9),
+    'MP5': _.weapon('MP5', types_1.WeaponType.SMG, types_1.Ammo._9),
+    'VECTOR': _.weapon('VECTOR', types_1.WeaponType.SMG, types_1.Ammo._45),
+};
+exports.shotguns = {
+    'REMINGTON 870': _.weapon('REMINGTON 870', types_1.WeaponType.SHOTGUN, types_1.Ammo._12),
+    'ORIGIN': _.weapon('ORIGIN', types_1.WeaponType.SHOTGUN, types_1.Ammo._12),
+};
+exports.pistols = {
+    'DEAGLE': _.weapon('DEAGLE', types_1.WeaponType.PISTOL, types_1.Ammo._50),
+};
+exports.lmgs = {
+    'PKM': _.weapon('PKM', types_1.WeaponType.LMG, types_1.Ammo._762),
+    'MG4': _.weapon('MG4', types_1.WeaponType.LMG, types_1.Ammo._556, true),
+};
+exports.ars = {
+    'M4': _.weapon('M4', types_1.WeaponType.AR, types_1.Ammo._556),
+    'LVOAC': _.weapon('LVOAC', types_1.WeaponType.AR, types_1.Ammo._556),
+    'FAMAS': _.weapon('FAMAS', types_1.WeaponType.AR, types_1.Ammo._556),
+    'AK': _.weapon('AK', types_1.WeaponType.AR, types_1.Ammo._762),
+    'GROZA': _.weapon('GROZA', types_1.WeaponType.AR, types_1.Ammo._762),
+    'AUG': _.weapon('AUG', types_1.WeaponType.AR, types_1.Ammo._556, true),
+};
+exports.snipers = {
+    'G28': _.weapon('G28', types_1.WeaponType.SNIPER, types_1.Ammo._762),
+    'MK12': _.weapon('MK12', types_1.WeaponType.SNIPER, types_1.Ammo._556),
+    'REMINGTON 700': _.weapon('REMINGTON 700', types_1.WeaponType.SNIPER, types_1.Ammo._556),
+    'MOSIN': _.weapon('MOSIN', types_1.WeaponType.SNIPER, types_1.Ammo._762),
+    'M200': _.weapon('M200', types_1.WeaponType.SNIPER, types_1.Ammo._408, true),
+    'VKS': _.weapon('VKS', types_1.WeaponType.SNIPER, types_1.Ammo._50, true),
+};
+exports.packs = {
+    GLIDER: _.pack('GLIDER', _.weapon('GLOCK', types_1.WeaponType.PISTOL, types_1.Ammo._9)),
+    BMX: _.pack('BMX', _.weapon('USP', types_1.WeaponType.PISTOL, types_1.Ammo._45)),
+    GRAPPLE: _.pack('GRAPPLE', _.weapon('CITORI', types_1.WeaponType.SHOTGUN, types_1.Ammo._12)),
+};
+
+
+/***/ }),
+
+/***/ "./source/data/maps.ts":
+/*!*****************************!*\
+  !*** ./source/data/maps.ts ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const dione = __webpack_require__(/*! ./dione */ "./source/data/dione.ts");
+const europa = __webpack_require__(/*! ./europa */ "./source/data/europa.ts");
+exports.MAPS = {
+    DIONE: {
+        name: 'Dione',
+        data: dione,
+    },
+    EUROPA: {
+        name: 'Europa',
+        data: europa,
+    },
+};
+exports.MAP_LIST = [
+    exports.MAPS.DIONE,
+    exports.MAPS.EUROPA,
+];
+
+
+/***/ }),
+
 /***/ "./source/index.ts":
 /*!*************************!*\
   !*** ./source/index.ts ***!
@@ -177,22 +246,144 @@ exports.packs = {
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const challenges_1 = __webpack_require__(/*! challenges */ "./source/challenges.ts");
+const maps_1 = __webpack_require__(/*! data/maps */ "./source/data/maps.ts");
+const storage_1 = __webpack_require__(/*! storage */ "./source/storage.ts");
 const buttonEl = document.querySelector('#generate');
 const outputEl = document.querySelector('#output');
+const mapEl = document.querySelector('#map');
+let mapPointer = maps_1.MAP_LIST.indexOf(storage_1.capsule.get('map'));
+let map = maps_1.MAP_LIST[mapPointer];
+mapEl.textContent = map.name;
 const renderWeapon = (weapon) => {
     return `<span class="weapon-ammo-${weapon.ammo} weapon-type-${weapon.type.toLowerCase()}">${weapon.name}</span>`;
 };
 const renderEntry = (challenge) => {
     const id = `<span class="id">#${challenges_1.newID()}:</span>`;
+    const map = `<span class="map map-${challenge.map.toLowerCase()}">[${challenge.map}]</span>`;
     const pack = `<span class="pack pack-type-${challenge.pack.name.toLowerCase()}"></span>`;
     const weapons = challenge.weapons.map(renderWeapon).join(', ');
-    return `<li class="entry">${pack} ${weapons}</li>`;
+    return `<li class="entry">${map} ${pack} ${weapons}</li>`;
 };
 buttonEl.addEventListener('click', (e) => {
     e.preventDefault();
-    const challenge = challenges_1.generate();
+    const challenge = challenges_1.generate(map);
     outputEl.innerHTML = `${renderEntry(challenge)}${outputEl.innerHTML}`;
 });
+mapEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    mapPointer++;
+    if (mapPointer >= maps_1.MAP_LIST.length) {
+        mapPointer = 0;
+    }
+    map = maps_1.MAP_LIST[mapPointer];
+    mapEl.textContent = map.name;
+});
+
+
+/***/ }),
+
+/***/ "./source/lib/capsule.ts":
+/*!*******************************!*\
+  !*** ./source/lib/capsule.ts ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class Capsule {
+    constructor(prefix, data) {
+        this.prefix = prefix || '';
+        this.keys = new Set();
+        this.setDefaults(data);
+        this.sync();
+    }
+    setDefaults(data = {}) {
+        for (let key in data) {
+            if (data.hasOwnProperty(key) && !this.has(key)) {
+                this.set(key, data[key]);
+            }
+        }
+    }
+    sync() {
+        for (let pre_key in localStorage) {
+            if (localStorage.hasOwnProperty(pre_key)) {
+                if (pre_key.indexOf(this.prefix) == 0) {
+                    let key = this.unprefixKey_(pre_key);
+                    this.keys.add(key);
+                }
+            }
+        }
+    }
+    unprefixKey_(pre_key) {
+        return pre_key.replace(new RegExp(`${this.prefix}_`), '');
+    }
+    prefixKey_(key) {
+        return `${this.prefix}_${key}`;
+    }
+    has(key) {
+        const pre_key = this.prefixKey_(key);
+        return localStorage.hasOwnProperty(pre_key);
+    }
+    set(key, value) {
+        const pre_key = this.prefixKey_(key);
+        this.keys.add(key);
+        try {
+            localStorage.setItem(pre_key, JSON.stringify({ '__data__': value }));
+        }
+        catch (e) {
+            console.warn(`Capsule didn't successfully save the '{ ${key}: ${value} }' pair, because the localStorage is full.`);
+        }
+    }
+    get(key, default_ = undefined) {
+        const pre_key = this.prefixKey_(key);
+        let value;
+        if (!localStorage.hasOwnProperty(pre_key)) {
+            return default_;
+        }
+        try {
+            let raw = JSON.parse(localStorage.getItem(pre_key));
+            value = raw.__data__;
+            return value;
+        }
+        catch (e) {
+            console.warn(`Capsule could not load the item with key: ${key}`);
+            return default_;
+        }
+    }
+    remove(key) {
+        const pre_key = this.prefixKey_(key);
+        this.keys.delete(key);
+        localStorage.removeItem(pre_key);
+    }
+    flush() {
+        Array.from(this.keys).forEach((key) => {
+            this.remove(key);
+        });
+    }
+}
+exports.Capsule = Capsule;
+
+
+/***/ }),
+
+/***/ "./source/storage.ts":
+/*!***************************!*\
+  !*** ./source/storage.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const capsule_1 = __webpack_require__(/*! lib/capsule */ "./source/lib/capsule.ts");
+const maps_1 = __webpack_require__(/*! data/maps */ "./source/data/maps.ts");
+const capsule = new capsule_1.Capsule(`roe-challenges`, {
+    map: maps_1.MAPS.DIONE.name,
+});
+exports.capsule = capsule;
 
 
 /***/ }),
@@ -202,89 +393,8 @@ buttonEl.addEventListener('click', (e) => {
   !*** ./source/types.ts ***!
   \*************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var Ammo;
-(function (Ammo) {
-    Ammo["_9"] = "9";
-    Ammo["_12"] = "12";
-    Ammo["_45"] = "45";
-    Ammo["_50"] = "50";
-    Ammo["_556"] = "556";
-    Ammo["_762"] = "762";
-    Ammo["_408"] = "408";
-})(Ammo = exports.Ammo || (exports.Ammo = {}));
-var WeaponType;
-(function (WeaponType) {
-    WeaponType["PISTOL"] = "PISTOL";
-    WeaponType["SHOTGUN"] = "SHOTGUN";
-    WeaponType["SMG"] = "SMG";
-    WeaponType["AR"] = "AR";
-    WeaponType["SNIPER"] = "SNIPER";
-})(WeaponType = exports.WeaponType || (exports.WeaponType = {}));
-
-
-/***/ }),
-
-/***/ "./source/utils.ts":
-/*!*************************!*\
-  !*** ./source/utils.ts ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randInt = (low, high) => Math.floor(Math.random() * (1 + high - low) + low);
-exports.randEntry = (list) => {
-    const i = exports.randInt(0, list.length - 1);
-    return list[i];
-};
-exports.randSubset = function (list, min, max) {
-    if (arguments.length === 2) {
-        max = min;
-    }
-    if (arguments.length === 1) {
-        min = max = exports.randInt(0, list.length - 1);
-    }
-    const length = exports.randInt(min, max);
-    const from = list.slice(0);
-    const sub = [];
-    for (let i = 0; i < length; i++) {
-        const rand = exports.randInt(0, from.length - 1);
-        const e = from.splice(rand, 1)[0];
-        sub.push(e);
-    }
-    return sub;
-};
-exports.randValue = (map) => {
-    const keys = Object.keys(map);
-    const i = exports.randInt(0, keys.length - 1);
-    const key = keys[i];
-    return map[key];
-};
-exports.randSubmap = function (map, min, max) {
-    const keys = Object.keys(map);
-    if (arguments.length === 2) {
-        max = min;
-    }
-    if (arguments.length === 1) {
-        min = max = exports.randInt(0, keys.length - 1);
-    }
-    const length = exports.randInt(min, max);
-    const from = keys.slice(0);
-    const sub = {};
-    for (let i = 0; i < length; i++) {
-        const rand = exports.randInt(0, from.length - 1);
-        const key = from.splice(rand, 1)[0];
-        sub[key] = map[key];
-    }
-    return sub;
-};
 
 
 /***/ })
--- source/index.ts
+++ source/index.ts
@@ -3,9 +3,19 @@ import {
   newID,
   generate,
 } from 'challenges';
+import {
+  MAPS,
+  MAP_LIST,
+} from 'data/maps';
+import { capsule } from 'storage';
 
 const buttonEl = document.querySelector('#generate');
 const outputEl = document.querySelector('#output');
+const mapEl = document.querySelector('#map');
+
+let mapPointer = MAP_LIST.indexOf(capsule.get('map'));
+let map = MAP_LIST[mapPointer];
+mapEl.textContent = map.name;
 
 const renderWeapon = (weapon: Weapon): string => {
   return `<span class="weapon-ammo-${weapon.ammo} weapon-type-${weapon.type.toLowerCase()}">${weapon.name}</span>`;
@@ -13,13 +23,24 @@ const renderWeapon = (weapon: Weapon): string => {
 
 const renderEntry = (challenge: Challenge): string => {
   const id = `<span class="id">#${newID()}:</span>`;
+  const map = `<span class="map map-${challenge.map.toLowerCase()}">[${challenge.map}]</span>`;
   const pack = `<span class="pack pack-type-${challenge.pack.name.toLowerCase()}"></span>`;
   const weapons = challenge.weapons.map(renderWeapon).join(', ');
-  return `<li class="entry">${pack} ${weapons}</li>`;
+  return `<li class="entry">${map} ${pack} ${weapons}</li>`;
 };
 
 buttonEl.addEventListener('click', (e) => {
   e.preventDefault();
-  const challenge: Challenge = generate();
+  const challenge: Challenge = generate(map);
   outputEl.innerHTML = `${renderEntry(challenge)}${outputEl.innerHTML}`;
+});
+
+mapEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  mapPointer++;
+  if(mapPointer >= MAP_LIST.length) {
+    mapPointer = 0;
+  }
+  map = MAP_LIST[mapPointer];
+  mapEl.textContent = map.name;
 });
